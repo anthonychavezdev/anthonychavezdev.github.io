4775c7fa50473ddf70e26513442179c46bfbe1a1
Anthony Chavez <anthony@anthonychavez.xyz>
2022-09-14 23:30:28 -0500

    Use the css from simplecss.org and switch to HTML 5
    By default, the Org publisher doesn't use HTML 5, so this commit
    switches to HTML 5, uses a preconfigured stylesheet from
    simplecss.org,
    and changes the preamble, content, and postamble CARS so that
    simplecss works with correct styling. It doesn't like the default divs
    that are inserted by preamble, content, and postamble.

--- build-site.el
+++ build-site.el
@@ -14,7 +14,14 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-(setq org-html-validation-link nil)
+;; Customize the HTML output
+(setq org-html-validation-link nil              ;; Don't show validation link
+      org-html-head-include-scripts nil         ;; Use custom scripts
+      org-html-head-include-default-style nil   ;; Use custom styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\""
+      org-html-divs '((preamble "header" "")
+                      (content "main" "")
+                      (postamble "footer" "")))
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -24,6 +31,21 @@
              :base-directory "./content"
              :publishing-directory "./public"
              :publishing-function 'org-html-publish-to-html
+             :html-preamble
+             "
+              <nav>
+                  <a href=\"/\">Home</a>
+                  <a href=\"/Notes/\">Notes</a>
+              </nav>
+              <h1>%t</h1>
+             "
+             :html-postamble
+             "
+              Created with: %c
+             "
+             :html-doctype "html5"
+             :html-html5-fancy t
+             :with-title nil
              :with-author nil        ;; Don't include author name
              :with-creator t         ;; Include Emacs and Org vesion in footer
              :with-toc nil           ;; Don't include table of contents
